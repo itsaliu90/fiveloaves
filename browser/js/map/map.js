c39c4c748d65c28832ba806644494b4f023e7f1b
Sean Kim <nogever@gmail.com>
2015-08-08 19:20:56 -0400

    added map

--- browser/js/map/map.js
+++ browser/js/map/map.js
@@ -1,6 +1,38 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('map', {
         url: '/map',
+        controller: 'MapController',
         templateUrl: 'js/map/map.html'
     });
 });
+
+app.controller('MapController', function ($scope) {
+    angular.extend($scope, {
+        center: {
+            lat: 40.7127,
+            lng: -74.0059,
+            zoom: 14
+        },
+        markers: {
+            nyc: {
+                lat: 40.7127,
+                lng: -74.0059,
+            }
+        },
+        layers: {
+            baselayers: {
+                mapbox_light: {
+                    name: 'Mapbox Light',
+                    url: 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
+                    type: 'xyz',
+                    layerOptions: {
+                        apikey: 'pk.eyJ1Ijoibm9nZXZlciIsImEiOiI0OGY5NzI2NjU0YTUwNzI5YzA2M2I4OTlmZDQ0NmM4MyJ9.3rZcWQ1wgEWB-rVs-VhE0A',
+                        mapid: 'nogever.aafce4fa'
+                    }
+                }
+            }
+        }
+    });
+});
+
+
