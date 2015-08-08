4cb25a52e414576acfdf7cb8a257eb91ded77018
normchou <normchou@users.noreply.github.com>
2015-08-08 17:31:39 -0400

    changed twillio index.js

--- server/app/configure/twilio/index.js
+++ server/app/configure/twilio/index.js
@@ -1,8 +1,13 @@
+var secrets = require("../../../secrets.js");
 
-// client.messages.create({
-//     body: "Wassup Norm!",
-//     to: "+16503031192",
-//     from: "+16193562837"
-// }, function(err, message) {
-//     process.stdout.write(message.sid);
-// });
+var accountSid = 'ACd2695be19b1e72ebf889d3e9486724cc';
+var authToken = secrets.twilioAuthToken;
+var client = require('twilio')(accountSid, authToken);
+
+client.messages.create({
+   body: "Wassup Norm!",
+   to: "+16503031192",
+   from: "+16193562837"
+}, function(err, message) {
+   process.stdout.write(message.sid);
+});
