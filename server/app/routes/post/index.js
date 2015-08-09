e10fa762fe55a7325c887935c93dda7e388c6c6c
Alex Liu <yuningalexliu@gmail.com>
2015-08-09 00:52:13 -0400

    create post sends sms

--- server/app/routes/post/index.js
+++ server/app/routes/post/index.js
@@ -1,7 +1,27 @@
 'use strict';
 var router = require('express').Router();
 var mongoose = require('mongoose');
+var secrets = require('../../../../secrets.js');
 var Post = mongoose.model('Post');
+var Registrant = mongoose.model('Registrant');
+
+// Helper method to send SMS
+var sendSMS = function(recipientPhoneNumber, postDescription) {
+	var accountSid = 'ACd2695be19b1e72ebf889d3e9486724cc';
+	var authToken = secrets.twilioAuthToken;
+	var client = require('twilio')(accountSid, authToken);
+	 
+	client.messages.create({
+	    body: postDescription,
+	    to: recipientPhoneNumber,
+	    from: "+16193562837"
+	}, function(err, message) {
+	    console.log("Message Sent");
+	    if (err) {
+	    	console.log(err);
+		}
+	});
+}
 
 router.get('/', function(req, res, next) {
 	Post.find({}, function(err, data) {
@@ -14,9 +34,24 @@ router.get('/:id', function(req, res, next) {
 });
 
 router.post('/', function(req, res, next) {
-	Post.create(req.body, function(err, inserted) {
+	Post.create(req.body, function(err, newPost) {
 		if (err) throw err;
-		res.json(inserted);
+		var zipCode = newPost.zipCode;
+
+		// Take zip code of that post and query Registrants for matches
+		Registrant.find({zipCodes: zipCode}).then(function(registrants) {
+
+		// Loop through registrants and call "sendMessage method"
+			var message = "Free food just posted! Organization: " + newPost.organizationName + "Description: " + newPost.description + "Address: " + newPost.address + "."
+
+			for (var i = 0; i < registrants.length; i++) {
+				sendSMS(registrants[i].phone, message);
+			}
+
+			res.json(registrants);
+		})
+
+		// Done!!!
 	});
 });
 
