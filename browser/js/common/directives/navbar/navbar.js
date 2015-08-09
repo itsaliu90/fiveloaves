e07c925fb8ee0635dd196fc86b2d3eeada71d7bd
Sean Kim <nogever@gmail.com>
2015-08-09 09:29:52 -0400

    layout of forms fixed

--- browser/js/common/directives/navbar/navbar.js
+++ browser/js/common/directives/navbar/navbar.js
@@ -8,7 +8,7 @@ app.directive('navbar', function ($rootScope, AuthService, AUTH_EVENTS, $state) 
 
             scope.items = [
                 // { label: 'About', state: 'about' },
-                { label: 'Map', state: 'map' },
+                { label: 'Browse', state: 'map' },
                 // { label: 'Documentation', state: 'docs' },
                 // { label: 'Members Only', state: 'membersOnly', auth: true },
                 { label: 'Send Alert', state: 'sendAlert', auth: true }
