c25accb0c2f380b7f8fa9becf2a9f0f538255e07
Alex Liu <yuningalexliu@gmail.com>
2015-08-08 22:05:35 -0400

    fix code error

--- server/app/routes/twilio/index.js
+++ server/app/routes/twilio/index.js
@@ -8,27 +8,27 @@ module.exports = router;
 router.post('/register', function(req, res) {
 
 
-	var keyword = req.body.message?
+	// var keyword = req.body.message?
 
 
 
 
-	switch(keyword) {
-	    case 'cg':
-	        code block
-	        break;
-	    case 'rg':
-	        code block
-	        break;
-	    case 'qt':
-	        code block
-	        break;
-	    case 'cg':
-	        code block
-	        break;
-	    default:
-	        default code block
-	}
+	// switch(keyword) {
+	//     case 'cg':
+	//         code block
+	//         break;
+	//     case 'rg':
+	//         code block
+	//         break;
+	//     case 'qt':
+	//         code block
+	//         break;
+	//     case 'cg':
+	//         code block
+	//         break;
+	//     default:
+	//         default code block
+	// }
 
 
 
